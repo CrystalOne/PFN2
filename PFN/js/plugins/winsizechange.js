2644d4b66dd93cc4d728c371b64c2fad5b9c0da3
CrystalOne <1063246579@qq.com>
2018-05-21 21:32:58 +0800

    完成更改屏幕大小插件

--- PFN/js/plugins/winsizechange.js
+++ PFN/js/plugins/winsizechange.js
@@ -1,22 +1,63 @@
-/*: 
- * @plugindesc 随时随地更改游戏屏幕尺寸
- * @author: Mandarava（鳗驼螺）
+/*:
+ * @plugindesc 修改游戏屏幕大小。
  * 
- * @param Screen width * @desc 游戏启动时游戏屏幕的宽度 
- * 默认值：816 
- * @default 816 
+ * @author: CJY
+ *
+ * @param Full Screen on startup
+ * @type boolean
+ * @on 开启全屏
+ * @off 关闭全屏
+ * @desc 在游戏启动时是否自动进入全屏
  * 
- * @param Screen height 
+ * @param Screen width
+ * @type number
+ * @desc 游戏启动时游戏屏幕的宽度
+ * 默认值：816
+ * @default 816
+ * 
+ * @param Screen height
+ * @type number
  * @desc 游戏启动时游戏屏幕的高度
- * 默认值：624 
- * @default 624 
+ * 默认值：624
+ * @default 624
+ * 
+ * @help
+ * 本插件用于改变窗口大小，但不会改变玩家的视角大小（是的，非常没用的插件- -）。
+ * 插件命令:
+ *  ChangeScreenSize 1024 768   #修改游戏屏幕尺寸为1024x768
+ *  RestoreScreenSize           #恢复为默认游戏屏幕尺寸（816x624）
+ *  Fullscreen true             #开启全屏
+ *  Fullscreen false            #退出全屏
  */
 
 var params = PluginManager.parameters("MND_ChangeScreenSize");
+var isFullScreen = String(params["Full Screen on startup"] || false);
 var screenWidth = Number(params["Screen width"]) || 816;
 var screenHeight = Number(params["Screen height"]) || 624;
 
-setScreenSize(screenWidth, screenHeight);
+if (isFullScreen != "false" && isFullScreen) Graphics._switchFullScreen();
+else setScreenSize(screenWidth, screenHeight);
+
+var _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+Game_Interpreter.prototype.pluginCommand = function (command, args) {
+    _Game_Interpreter_pluginCommand.call(this, command, args);
+
+    switch (command) {
+        case "ChangeScreenSize"://修改分辨率，插件命令格式：ChangeScreenSize [width] [height]
+            var _screenWidth = Number(args[0]) || 816;
+            var _screenHeight = Number(args[1]) || 624;
+            setScreenSize(_screenWidth, _screenHeight);
+            break;
+        case "RestoreScreenSize"://恢复为默认分辨率，插件命令格式：RestoreScreenSize
+            setScreenSize(816, 624);
+            break;
+        case "Fullscreen"://进入或退出全屏，插件命令格式：Fullscreen true/false
+            if (args[0] != "false" && args[0]) Graphics._requestFullScreen();
+            else Graphics._cancelFullScreen();
+            break;
+        default: break;
+    }
+}
 
 function setScreenSize(screenWidth, screenHeight) {
     var deltaWidth = screenWidth - window.innerWidth;
